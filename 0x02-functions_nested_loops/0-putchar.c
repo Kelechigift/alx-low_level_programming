ae51418822f0f346b3468ca30659a0957de6a321
Kelechigift <kelechigift22@gmail.com>
2023-02-22 16:45:22 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char n[] = "_putchar";
+	int i;
+
+	for (i = 0; n[i]; i++)
+		_putchar(n[i]);
+	_putchar('\n');
+	return (0);
+}
