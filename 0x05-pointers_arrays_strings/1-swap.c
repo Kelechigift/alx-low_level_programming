2968dfe0782565713498987911157bf59ddbc663
Kelechigift <kelechigift22@gmail.com>
2023-03-01 08:49:54 -0800

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swap values
+ * @a: parameter to be printed
+ * @b: parameter to be printed
+ *  Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
