881db7c6649e1731788a41d57a4ece04d865437d
Kelechigift <kelechigift22@gmail.com>
2023-03-03 07:28:56 -0800

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, c_res;
+	int i;
 
-	for (i = 0; s1[i] != '\0')
-		i++;
-	for (j = 0; s2[j] != '\0')
-		j++;
-	if (s1[i] == s2[j])
+	for (i = 0; s1[i] != '\0' || s2[i] != 0; i++)
 	{
-		continue;
-	}
-	else
-	{
-		c_res = s1[i] - s2[j];
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
+		}
 	}
 	return (0);
 }
