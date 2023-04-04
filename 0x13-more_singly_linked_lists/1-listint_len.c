8079ad1c0776ec0e6a0c739efc0356fe51f2c4e6
Kelechigift <kelechigift22@gmail.com>
2023-04-04 07:20:06 -0700

    Print list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - function return number of elements in linked listint_t list
+ * @h: pointer points to the head of list
+ * Return: 0
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t cnt = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		cnt++;
+	}
+	return (cnt)
+}
